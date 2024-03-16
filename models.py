653460115965a966c1f96cf248c705b6e1358143
pearlshel <hershelmikhail@gmail.com>
2024-03-16 12:11:35 +0800

    Add database

--- models.py
+++ models.py
@@ -1,66 +1,69 @@
-from typing import List
+from typing import List, Optional
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 import random
 
 
 # TODO: Implement a database to store user and expense data
 # TODO: Organize models into separate files
-class User:
-    def __init__(self, name: str, id: int):
-        """
-        Initializes a User object with a name, id, and an empty list of expenses.
 
-        Parameters
-        ----------
-            name : str
-                a string representing the user's name
-            id : int
-                a int representing the user's id
-        """
-        self.name = name
-        # TODO: This id is temporary. Use database to generate unique ids.
-        self.id = id
-        self.expenses = []
-
-    def __str__(self) -> str:
-        return self.name
+engine = create_engine("sqlite+pysqlite:///test.db", echo=True)
 
 
-class Expense:
-    def __init__(self, title: str, amount: float, paid_by: User, equally_split: bool):
-        """
-        Initializes an Expense object with title, amount, paid_by, and settled attributes.
+class Base(DeclarativeBase):
+    """
+    Base class for mapping
+    """
 
-        Parameters
-        ----------
-            title : str
-                a string representing the title of the expense
-            amount : float
-                a float representing the amount of the expense
-            paid_by : int
-                a int representing the id of a User object who paid for the expense
-            equally_split : bool
-                a boolean representing whether the expense is equally split
-            settled : bool
-                a boolean representing whether the expense has been settled
-
-        """
-        self.title = title
-        self.amount = amount
-        self.paid_by = paid_by
-        self.equally_split = equally_split
-        self.settled = False
-
-        self.user_paid_by = self.__get_user_paid_by()
-
-    def __get_user_paid_by(self) -> User:
-        # TODO: Implement user search in database
-        for user in users:
-            if user.id == int(self.paid_by):
-                return user
+    pass
 
 
-expenses = []
-users = [
-    User("ARPM", id=1),
-    User("HMF", id=2),
-]
+# user_group_association = Table(
+#     "user_group_association",
+#     Base.metadata,
+#     Column("user_id", Integer, ForeignKey("users.id")),
+#     Column("group_id", Integer, ForeignKey("groups.id")),
+# )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+
+    groups: Mapped[List["Group"]] = relationship("Group", back_populates="members")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r})"
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+    members: Mapped[List["User"]] = relationship("User", back_populates="groups")
+
+    def __repr__(self) -> str:
+        return f"Group(id={self.id!r}, name={self.name!r})"
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    owed = mapped_column(ForeignKey("users.id"))
+    currency: Mapped[str] = mapped_column(String(3))
+    value: Mapped[float] = mapped_column()
+
+    ower: Mapped[List["User"]] = relationship("User")
+
+
+Base.metadata.create_all(engine)
