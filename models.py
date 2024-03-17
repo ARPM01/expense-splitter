dc5aa22318b0e0f31070a7de17b830b990c525fd
pearlshel <hershelmikhail@gmail.com>
2024-03-17 22:05:26 +0800

    Modify app.py

--- models.py
+++ models.py
@@ -9,66 +9,45 @@ import random
 
 # TODO: Implement a database to store user and expense data
 # TODO: Organize models into separate files
-class User:
-    def __init__(self, name: str, id: int):
-        """
-        Initializes a User object with a name, id, and an empty list of expenses.
+class Base(DeclarativeBase):
+    """
+    Base class for mapping
+    """
 
-        Parameters
-        ----------
-            name : str
-                a string representing the user's name
-            id : int
-                a int representing the user's id
-        """
-        self.name = name
-        # TODO: This id is temporary. Use database to generate unique ids.
-        self.id = id
-        self.expenses = []
-
-    def __str__(self) -> str:
-        return self.name
+    pass
 
 
-class Expense:
-    def __init__(self, title: str, amount: float, paid_by: User, equally_split: bool):
-        """
-        Initializes an Expense object with title, amount, paid_by, and settled attributes.
+class User(Base):
+    __tablename__ = "users"
 
-        Parameters
-        ----------
-            title : str
-                a string representing the title of the expense
-            amount : float
-                a float representing the amount of the expense
-            paid_by : int
-                a int representing the id of a User object who paid for the expense
-            equally_split : bool
-                a boolean representing whether the expense is equally split
-            settled : bool
-                a boolean representing whether the expense has been settled
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
 
-        """
-        self.title = title
-        self.amount = amount
-        self.paid_by = paid_by
-        self.equally_split = equally_split
-        self.settled = False
+    groups: Mapped[List["Group"]] = relationship("Group", back_populates="members")
 
-    def get_user_paid_by(self) -> User:
-        """
-        Returns the User object who paid for the expense.
-        """
-        for user in users_list:
-            if user.id == int(self.paid_by):
-                return user
-
-    def __str__(self) -> str:
-        return f"{self.title} - {self.amount} - {self.paid_by} - {self.equally_split}"
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r})"
 
 
-expenses_list = []
-users_list = [
-    User("ARPM", id=1),
-    User("HMF", id=2),
-]
+# class Group(Base):
+#     __tablename__ = "groups"
+
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(30))
+
+#     members: Mapped[List["User"]] = relationship("User", back_populates="groups")
+
+#     def __repr__(self) -> str:
+#         return f"Group(id={self.id!r}, name={self.name!r})"
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    owed = mapped_column(ForeignKey("users.id"))
+    currency: Mapped[str] = mapped_column(String(3))
+    value: Mapped[float] = mapped_column()
+    split: Mapped[bool] = mapped_column()
