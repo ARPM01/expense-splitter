f6746c5bb3184884e1e47bb3cda2f12d0633fbc0
Adonais Ray Maclang <apmaclang@up.edu.ph>
2024-07-18 23:26:39 +0800

    Add dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <HomePage />
+      </ThemeProvider>
     </div>
   );
 }
